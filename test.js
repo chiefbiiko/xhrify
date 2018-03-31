1e38d8d448d769a6d5e0f976e53e04363f3039f9
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-03-31 19:16:20 +0200

    another test

--- test.js
+++ test.js
@@ -24,3 +24,15 @@ tape('xhrify - HEAD', function (t) {
     t.end()
   })
 })
+
+tape('xhrify - error', function () {
+  var conf = {
+    url: 'https://raw.githubusercontent.com/chiefbiiko/siphash24-stream/master/index.js',
+    method: 'post'
+    // ,data: 'fraud'
+  }
+  xhrify(conf, function (err, res) {
+    t.ok(err, 'got some error')
+    t.end()
+  })
+})
